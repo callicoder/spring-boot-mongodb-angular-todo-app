d4d30ed236c87daba7d96016b688c887560ff79e
Rajeev Kumar Singh <rajeevs@flock.co>
2018-03-04 10:12:03 +0530

    Using javax NotBlank constraint

--- spring-boot-backend/src/main/java/com/example/todoapp/models/Todo.java
+++ spring-boot-backend/src/main/java/com/example/todoapp/models/Todo.java
@@ -1,9 +1,9 @@
 package com.example.todoapp.models;
 
 import java.util.Date;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
